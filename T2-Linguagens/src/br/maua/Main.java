189b84f9bf04460c083a399564426336be9bb05f
Renan Reschke <renanreschke@hotmail.com>
2021-06-19 16:30:00 -0300

    Javadoc e comentarios Main

--- T2-Linguagens/src/br/maua/Main.java
+++ T2-Linguagens/src/br/maua/Main.java
@@ -2,14 +2,22 @@ package br.maua;
 
 import br.maua.models.Sistema;
 
+/**
+ * Classe para executar o sistema
+ * @author Renan Scheidt Reschke RA: 19.02009-0
+ * @author Felipe Freitas Villani RA: 19.01370-0
+ * @version JDK 15
+ */
 public class Main {
 
     public static void main(String[] args) {
-	//F7 Passou por aqui
+        //Cria uma instancia do sistema
         Sistema sistema = new Sistema();
+        //Tenta iniciar o sistema
         try {
             sistema.iniciar();
         }
+        //Caso algum erro ocorra na inicializacao
         catch (Exception e){
             System.out.println("Ocorreu algum erro");
         }
