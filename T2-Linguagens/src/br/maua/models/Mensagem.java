0ef9963c1e60bec82c3e590e8a348ec86173d15b
F7Villani <felipevillani.2000@hotmail.com>
2021-06-19 12:09:10 -0300

    Classe Mensagem

---
+++ T2-Linguagens/src/br/maua/models/Mensagem.java
@@ -0,0 +1,25 @@
+package br.maua.models;
+
+import java.util.Random;
+
+public class Mensagem {
+    String[] bancoMensagens = new String[]{"Não se esqueça de beber água.",
+            "Seja estranho. Seja aleatório. Seja quem você é. Porque você nunca sabe quem amaria a pessoa que você esconde.",
+            "A vida é um caos aleatório, ordenada pelo tempo.",
+            "A verdade, é que dói lembrar dela.",
+            "O aleatório não existe, nosso cérebro sempre toma decisões mesmo que ocultas.",
+            "A vida e uma caixa preta nunca saberemos o seu real significado.",
+            "Não deveríamos temer a morte, mais sim a vida.",
+            "O ruim de mentir, com tempo você acaba acreditando nas suas próprias mentiras.",
+            "Cada instante é sempre.",
+            "Quem tenta fazer todo mundo feliz acaba se sentido mais sozinho.",
+            "Seja bondoso com seus amigos." };
+
+    public Mensagem(){};
+
+    public String gerarMensagem(){
+        Random random = new Random();
+        int i = random.nextInt(bancoMensagens.length);
+        return bancoMensagens[i];
+    }
+}
