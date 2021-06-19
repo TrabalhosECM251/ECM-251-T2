99e3cd91d1c709f123c984a7bcb8ec7dfc5d86c9
Renan Reschke <renanreschke@hotmail.com>
2021-06-19 11:32:41 -0300

    pequenas alt

--- T2-Linguagens/src/br/maua/interfaces/Apresentacao.java
+++ T2-Linguagens/src/br/maua/interfaces/Apresentacao.java
@@ -1,11 +1,8 @@
 package br.maua.interfaces;
 
-
-import br.maua.models.Membro;
-
 /**
  * Interface para apresentar um membro
  */
 public interface Apresentacao {
-    public void apresentar(Membro membro);
+    public void apresentar();
 }
--- T2-Linguagens/src/br/maua/interfaces/PostarMensagem.java
+++ T2-Linguagens/src/br/maua/interfaces/PostarMensagem.java
@@ -1,8 +1,10 @@
 package br.maua.interfaces;
 
+import br.maua.enums.HorarioDeAtividade;
+
 /**
  * Interface para um membro postar uma mensagem
  */
 public interface PostarMensagem {
-    public void postarMensagem(String mensagem);
+    public void postarMensagem(String mensagem, HorarioDeAtividade hora);
 }
