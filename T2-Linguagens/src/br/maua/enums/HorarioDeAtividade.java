7834a25815dd9dfd14c21d8fddcb130037c614ce
Renan Reschke <renanreschke@hotmail.com>
2021-06-19 11:33:05 -0300

    pequenas alt

--- T2-Linguagens/src/br/maua/enums/HorarioDeAtividade.java
+++ T2-Linguagens/src/br/maua/enums/HorarioDeAtividade.java
@@ -3,8 +3,8 @@ package br.maua.enums;
 /**
  * Enum dos tipos de horario de trabalho
  * EXTRA = hora extra de trabalho
- * NORMAL = hora normal de trabalho
+ * REGULAR = hora normal de trabalho
  */
 public enum HorarioDeAtividade {
-    EXTRA,NORMAL;
+    EXTRA,REGULAR;
 }
