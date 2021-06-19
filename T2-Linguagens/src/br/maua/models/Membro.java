cdc8916e2f8112a8a98f115516e07cc559c6ee43
Renan Reschke <renanreschke@hotmail.com>
2021-06-19 11:06:39 -0300

    Classe Membro

--- T2-Linguagens/src/br/maua/models/Membro.java
+++ T2-Linguagens/src/br/maua/models/Membro.java
@@ -1,4 +1,51 @@
 package br.maua.models;
 
-abstract class Membro {
+import br.maua.enums.TipoDeMembro;
+import br.maua.interfaces.Apresentacao;
+import br.maua.interfaces.PostarMensagem;
+
+/**
+ * Classe abstrata para representar um membro
+ */
+abstract class Membro implements PostarMensagem,Apresentacao {
+    private String nome;
+    private String email;
+    private String assinaturaRegular;
+    private String assinaturaExtra;
+    private TipoDeMembro tipoDeMembro;
+
+    /**
+     * Construtor de um membro
+     * @param nome nome do membro
+     * @param email email do membro
+     * @param tipoDeMembro tipo de membro
+     */
+    public Membro(String nome, String email, TipoDeMembro tipoDeMembro){
+        this.nome = nome;
+        this.email = email;
+        this.tipoDeMembro = tipoDeMembro;
+    }
+
+    /**
+     * Getters
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAssinaturaRegular() {
+        return assinaturaRegular;
+    }
+
+    public String getAssinaturaExtra() {
+        return assinaturaExtra;
+    }
+
+    public TipoDeMembro getTipoDeMembro() {
+        return tipoDeMembro;
+    }
 }
